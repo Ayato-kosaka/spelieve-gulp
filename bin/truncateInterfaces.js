03dce59359a53aaf5b45a2a8c07978da395dc21d
Ayato-kosaka <kosaka.ayato@gmail.com>
2022-10-01 09:04:39 +0000

    modify

--- bin/truncateInterfaces.js
+++ bin/truncateInterfaces.js
@@ -3,4 +3,4 @@
 'use strict'
 import { execSync }  from 'child_process';
 console.log(execSync('rm -r -f Interface').toString())
-console.log(execSync('cd node_modules/spelieve-gulp && npx gulp Interfaces --arg1 ../../').toString())
+console.log(execSync(`cd node_modules/spelieve-gulp && npx gulp Interfaces --arg1 ${execSync('pwd').toString()}`).toString())
